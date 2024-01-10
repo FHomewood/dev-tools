0ff0efb36dbdf41f263cab54a66e23eed00a3216
DV | Frankie Homewood <frankie.homewood@data-vault.com>
2024-01-10 10:45:58 +0000

    Development environment script v0.1.1 - Add `-Playground -p` parameter - Format variable names to be snake_case

--- devenv.ps1
+++ devenv.ps1
@@ -1,32 +1,40 @@
 # Copyright 2024 Frankie Homewood <F.Homewood@outlook.com>
+# Create new development environment
+# v0.1.1
+[CmdletBinding()]
+param (
+    [switch] $Playground
+)
 
 ### ~~~~ CONFIG ~~~~ ###
-$pythonver = "3.11.5"
-$rootdir = "~\Development\"
+$python_version = "3.11.5"
+$development_dir = "~\Development\"
+$playground_dir = "~\Playground\"
 ### ~~~~~~~~~~~~~~~~ ###
 
-
-$envnum = Get-Random -Minimum 10000 -Maximum 99999
-
-Write-Host "~~~ Building Environment #$envnum ~~~" -ForegroundColor DarkGreen
+$env_num = Get-Random -Minimum 10000 -Maximum 99999
+$env_type = "Development"
+if ($Playground) {$env_type = "Playground"}
+Write-Host "~~~ Building $env_type Environment #$env_num ~~~" -ForegroundColor DarkGreen
 Write-Host "  - Setting up..." -ForegroundColor Blue
-$isSuccessful = $true
-$workdir = Get-Location
-$targetdir = "project_$envnum\"
-$envpath = $rootdir + $targetdir
+$is_successful = $true
+$work_dir = Get-Location
+$target_dir = "project_$env_num\"
+$env_path = $development_dir + $target_dir
+if ($Playground) {$env_path = $playground_dir + $target_dir}
 
 Write-Host "  - Building path..." -ForegroundColor Blue
-$null = New-Item -Path $rootdir -Name $targetdir -ItemType Directory
-$null = New-Item -Path $envpath -Name "src" -ItemType Directory
-$null = New-Item -Path $envpath -Name "tests" -ItemType Directory
-$null = New-Item -Path "$envpath\src" -Name "__init__.py" -ItemType File
-$null = New-Item -Path "$envpath\src" -Name "__main__.py" -ItemType File
-$null = New-Item -Path "$envpath\tests" -Name "__init__.py" -ItemType File
-$null = New-Item -Path $envpath -Name "README.md" -ItemType File
+$null = New-Item -Path $rootdir -Name $target_dir -ItemType Directory
+$null = New-Item -Path $env_path -Name "src" -ItemType Directory
+$null = New-Item -Path $env_path -Name "tests" -ItemType Directory
+$null = New-Item -Path "$env_path\src" -Name "__init__.py" -ItemType File
+$null = New-Item -Path "$env_path\src" -Name "__main__.py" -ItemType File
+$null = New-Item -Path "$env_path\tests" -Name "__init__.py" -ItemType File
+$null = New-Item -Path $env_path -Name "README.md" -ItemType File
 
 Write-Host "  - Finding path..." -ForegroundColor Blue
-$null = Set-Location $envpath
-$abspath = Get-Location
+$null = Set-Location $env_path
+$abs_path = Get-Location
 
 try {
     Write-Host "  - Initializing git repository..." -ForegroundColor Blue
@@ -40,34 +48,34 @@ try {
     Write-Host "      Upgraded pip" -ForegroundColor DarkBlue
     $null = pip install poetry
     Write-Host "      Installed poetry" -ForegroundColor DarkBlue
-    &poetry init -n `
+    $null = poetry init -n `
             --name src `
-            --python $pythonver `
+            --python $python_version `
             --author "Frankie Homewood <Frankie.Homewood@data-vault.com>" `
             --description `
-            "Project ID #${envnum}: Authored by Frankie Homewood in association with Business Thinking Ltd. (dba Datavault)" `
+            "Project ID #${env_num}: Authored by Frankie Homewood in association with Business Thinking Ltd. (dba Datavault)" `
 
     Write-Host "      Initialized project" -ForegroundColor DarkBlue
     $null = poetry install
     Write-Host "      Locked pyproject.toml" -ForegroundColor DarkBlue
 }
 catch {
-    $isSuccessful = $false
+    $is_successful = $false
     
-    $null = Set-Location $workdir
-    $null = Remove-Item  $abspath -Recurse -Force
+    $null = Set-Location $work_dir
+    $null = Remove-Item  $abs_path -Recurse -Force
 
-    Write-Host "An error occurred creating Environment #$envnum" -ForegroundColor Red
+    Write-Host "An error occurred creating Environment #$env_num" -ForegroundColor Red
     Write-Warning -Message $Error[0].Exception.Message
 }
 finally {
     Write-Host "  - Cleaning up..." -ForegroundColor Blue
     $null = deactivate
-    $null = Set-Location $workdir
+    $null = Set-Location $work_dir
 }
 
-if ($isSuccessful) {
-    $null = code $abspath
+if ($is_successful) {
+    $null = code $abs_path
     Write-Host "Done!" -ForegroundColor Green
 }
 [System.GC]::Collect()
