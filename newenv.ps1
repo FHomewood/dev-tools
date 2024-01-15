f8b0546638c5b1645ac5ebd6a57a5f71169d220f
Frankie Homewood <f.homewood@outlook.com>
2024-01-15 11:13:04 +0000

    Development environment script v0.1.3 - Pull metadata from env - Fix error handling getting lost in temp dirs - Move Info Flags to the bottom of the script

--- newenv.ps1
+++ newenv.ps1
@@ -8,34 +8,99 @@ param (
     [switch] $Help,
     [switch] $Playground
 )
-$version_number = "v0.1.2"
+
+### ~~~~ METADATA ~~~~ ###
+$version_number = "v0.1.3"
 $script_name = 'newenv'
-### ==== PARAMETERS ==== ###
 
 ### ~~~~ CONFIG ~~~~ ###
 $python_version = "3.11.5"
 $development_dir = "~\Development\"
 $playground_dir = "~\Playground\"
-$first_name = "Frankie"
-$last_name = "Homewood"
-### ==== CONFIG ==== ###
+$first_name = [Environment]::GetEnvironmentVariable("config_first_name", "Machine")
+$last_name = [Environment]::GetEnvironmentVariable("config_last_name", "Machine")
 
 ### ~~~~ SETUP ~~~~ ###
 $$ = ' $'
 $env_num = Get-Random -Minimum 10000 -Maximum 99999
 $env_type = "Development"
 $env_dir = $development_dir
-$is_successful = $true
-$work_dir = Get-Location
+$is_successful = $false
+$venv_exists = $false
+$error_message = ''
+$init_dir = Get-Location
 $target_dir = "project_$env_num\"
 $env_path = $development_dir + $target_dir
 
 if ($Playground) {
     $env_type = "Playground"
     $env_dir = $playground_dir
-    $env_path = $env_dir + $target_dir}
-### ==== SETUP ==== ###
+    $env_path = $env_dir + $target_dir
+}
 
+### ~~~~ BUILD ~~~~ ###
+Write-Host "~~~ Building $env_type Environment #$env_num ~~~" -ForegroundColor DarkGreen
+Write-Host "  - Building path..." -ForegroundColor Cyan
+$null = New-Item -Path $env_dir -Name $target_dir -ItemType Directory
+$null = New-Item -Path $env_path -Name "src" -ItemType Directory
+$null = New-Item -Path $env_path -Name "tests" -ItemType Directory
+$null = New-Item -Path "$env_path\src" -Name "__init__.py" -ItemType File
+$null = New-Item -Path "$env_path\src" -Name "__main__.py" -ItemType File
+$null = New-Item -Path "$env_path\tests" -Name "__init__.py" -ItemType File
+$null = New-Item -Path $env_path -Name "README.md" -ItemType File
+
+Write-Host "  - Finding path..." -ForegroundColor Cyan
+$null = Set-Location $env_path
+$abs_path = Get-Location
+
+try {
+    Write-Host "  - Initializing git repository..." -ForegroundColor Cyan
+    $null = git init
+    
+    Write-Host "  - Building Python environment..." -ForegroundColor Cyan
+    $null = pyenv exec python -m virtualenv .venv
+    $venv_exists = $true
+    Write-Host "      Created .\.venv\" -ForegroundColor DarkCyan
+    $null = . .\.venv\Scripts\activate
+    $null = pip install --upgrade pip
+    Write-Host "      Upgraded pip" -ForegroundColor DarkCyan
+    $null = pip install poetry
+    Write-Host "      Installed poetry" -ForegroundColor DarkCyan
+    $null = poetry init -n `
+            --name src `
+            --python $python_version `
+            --author "$first_name $last_name <$first_name.$last_name@data-vault.com>" `
+            --description `
+            "Project ID #${env_num}: Authored by $first_name $last_name in association with Business Thinking Ltd. (dba Datavault)" `
+
+    Write-Host "      Initialized project" -ForegroundColor DarkCyan
+    $null = poetry install
+    Write-Host "      Locked pyproject.toml" -ForegroundColor DarkCyan
+    $is_successful = $true
+}
+catch {
+    $error_message = $Error[0].Exception.Message
+}
+finally {
+    Write-Host "  - Cleaning up..." -ForegroundColor Cyan
+    if ($venv_exists){
+        Write-Host "      Deactivating python virtual environment" -ForegroundColor Cyan
+        $null = deactivate
+    }
+    Write-Host "      returning to init_dir" -ForegroundColor Cyan
+    $null = Set-Location $init_dir
+
+    if ($is_successful) {
+        $null = code $abs_path
+        Write-Host "Done!" -ForegroundColor Green
+    }
+    else {
+        Write-Host "  - Restoring..." -ForegroundColor Cyan
+        $null = Remove-Item  $abs_path -Recurse -Force
+        if (!$error_message) { $error_message = "An unknown error occurred"}
+        Write-Warning -Message "$error_message"
+    }
+}
 
 ### ~~~~ INFO FLAGS ~~~~ ###
 if ($Version) {
@@ -100,67 +165,6 @@ Parameter flags can be supplied with the command to adjust the script's behaviou
     Write-Output $table
     break
 }
-### ==== INFO FLAGS ==== ###
 
-### ~~~~ BUILD ~~~~ ###
-Write-Host "~~~ Building $env_type Environment #$env_num ~~~" -ForegroundColor DarkGreen
-Write-Host "  - Building path..." -ForegroundColor Blue
-$null = New-Item -Path $env_dir -Name $target_dir -ItemType Directory
-$null = New-Item -Path $env_path -Name "src" -ItemType Directory
-$null = New-Item -Path $env_path -Name "tests" -ItemType Directory
-$null = New-Item -Path "$env_path\src" -Name "__init__.py" -ItemType File
-$null = New-Item -Path "$env_path\src" -Name "__main__.py" -ItemType File
-$null = New-Item -Path "$env_path\tests" -Name "__init__.py" -ItemType File
-$null = New-Item -Path $env_path -Name "README.md" -ItemType File
-
-Write-Host "  - Finding path..." -ForegroundColor Blue
-$null = Set-Location $env_path
-$abs_path = Get-Location
-
-try {
-    Write-Host "  - Initializing git repository..." -ForegroundColor Blue
-    $null = git init
-    
-    Write-Host "  - Building Python environment..." -ForegroundColor Blue
-    $null = pyenv exec python -m virtualenv .venv
-    Write-Host "      Created .\.venv\" -ForegroundColor DarkBlue
-    $null = . .\.venv\Scripts\activate
-    $null = pip install --upgrade pip
-    Write-Host "      Upgraded pip" -ForegroundColor DarkBlue
-    $null = pip install poetry
-    Write-Host "      Installed poetry" -ForegroundColor DarkBlue
-    $null = poetry init -n `
-            --name src `
-            --python $python_version `
-            --author "$first_name $last_name <$first_name.$last_name@data-vault.com>" `
-            --description `
-            "Project ID #${env_num}: Authored by $first_name $last_name in association with Business Thinking Ltd. (dba Datavault)" `
-
-    Write-Host "      Initialized project" -ForegroundColor DarkBlue
-    $null = poetry install
-    Write-Host "      Locked pyproject.toml" -ForegroundColor DarkBlue
-}
-catch {
-
-    Write-Host "An error occurred creating Environment #$env_num" -ForegroundColor Red
-    Write-Warning -Message $Error[0].Exception.Message
-    
-    Write-Host "  - Restoring..." -ForegroundColor Blue
-    $is_successful = $false
-    
-    $null = Set-Location $work_dir
-    $null = Remove-Item  $abs_path -Recurse -Force
-}
-finally {
-    Write-Host "  - Cleaning up..." -ForegroundColor Blue
-    $null = deactivate
-    $null = Set-Location $work_dir
-}
-
-if ($is_successful) {
-    $null = code $abs_path
-    Write-Host "Done!" -ForegroundColor Green
-}
-### ==== BUILD ==== ###
-
+### ~~~~ GARBAGE COLLECTION ~~~~ ###
 [System.GC]::Collect()
