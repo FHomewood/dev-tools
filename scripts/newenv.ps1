e7e18c8670fac99baf42c5dadd1fae1f5e704045
Frankie Homewood <f.homewood@outlook.com>
2024-01-15 16:23:27 +0000

    Development environment script v0.2.0 - Refactor the info flags into switch cases with PS functions - Remove incorrect use of $$ - Complete help descriptions

--- scripts/newenv.ps1
+++ scripts/newenv.ps1
@@ -10,7 +10,7 @@ param (
 )
 
 ### ~~~~ METADATA ~~~~ ###
-$version_number = "v0.1.3"
+$version_number = "v0.2.0"
 $script_name = 'newenv'
 
 ### ~~~~ CONFIG ~~~~ ###
@@ -21,7 +21,6 @@ $first_name = [Environment]::GetEnvironmentVariable("config_first_name", "Machin
 $last_name = [Environment]::GetEnvironmentVariable("config_last_name", "Machine")
 
 ### ~~~~ SETUP ~~~~ ###
-$$ = ' $'
 $env_num = Get-Random -Minimum 10000 -Maximum 99999
 $env_type = "Development"
 $env_dir = $development_dir
@@ -38,76 +37,74 @@ if ($Playground) {
     $env_path = $env_dir + $target_dir
 }
 
-### ~~~~ BUILD ~~~~ ###
-Write-Host "~~~ Building $env_type Environment #$env_num ~~~" -ForegroundColor DarkGreen
-Write-Host "  - Building path..." -ForegroundColor Cyan
-$null = New-Item -Path $env_dir -Name $target_dir -ItemType Directory
-$null = New-Item -Path $env_path -Name "src" -ItemType Directory
-$null = New-Item -Path $env_path -Name "tests" -ItemType Directory
-$null = New-Item -Path "$env_path\src" -Name "__init__.py" -ItemType File
-$null = New-Item -Path "$env_path\src" -Name "__main__.py" -ItemType File
-$null = New-Item -Path "$env_path\tests" -Name "__init__.py" -ItemType File
-$null = New-Item -Path $env_path -Name "README.md" -ItemType File
-
-Write-Host "  - Finding path..." -ForegroundColor Cyan
-$null = Set-Location $env_path
-$abs_path = Get-Location
-
-try {
-    Write-Host "  - Initializing git repository..." -ForegroundColor Cyan
-    $null = git init
+function Build{
+    Write-Host "~~~ Building $env_type Environment #$env_num ~~~" -ForegroundColor DarkGreen
+    Write-Host "  - Building path..." -ForegroundColor Cyan
+    $null = New-Item -Path $env_dir -Name $target_dir -ItemType Directory
+    $null = New-Item -Path $env_path -Name "src" -ItemType Directory
+    $null = New-Item -Path $env_path -Name "tests" -ItemType Directory
+    $null = New-Item -Path "$env_path\src" -Name "__init__.py" -ItemType File
+    $null = New-Item -Path "$env_path\src" -Name "__main__.py" -ItemType File
+    $null = New-Item -Path "$env_path\tests" -Name "__init__.py" -ItemType File
+    $null = New-Item -Path $env_path -Name "README.md" -ItemType File
     
-    Write-Host "  - Building Python environment..." -ForegroundColor Cyan
-    $null = pyenv exec python -m virtualenv .venv
-    $venv_exists = $true
-    Write-Host "      Created .\.venv\" -ForegroundColor DarkCyan
-    $null = . .\.venv\Scripts\activate
-    $null = pip install --upgrade pip
-    Write-Host "      Upgraded pip" -ForegroundColor DarkCyan
-    $null = pip install poetry
-    Write-Host "      Installed poetry" -ForegroundColor DarkCyan
-    $null = poetry init -n `
-            --name src `
-            --python $python_version `
-            --author "$first_name $last_name <$first_name.$last_name@data-vault.com>" `
-            --description `
-            "Project ID #${env_num}: Authored by $first_name $last_name in association with Business Thinking Ltd. (dba Datavault)" `
-
-    Write-Host "      Initialized project" -ForegroundColor DarkCyan
-    $null = poetry install
-    Write-Host "      Locked pyproject.toml" -ForegroundColor DarkCyan
-    $is_successful = $true
-}
-catch {
-    $error_message = $Error[0].Exception.Message
-}
-finally {
-    Write-Host "  - Cleaning up..." -ForegroundColor Cyan
-    if ($venv_exists){
-        Write-Host "      Deactivating python virtual environment" -ForegroundColor Cyan
-        $null = deactivate
+    Write-Host "  - Finding path..." -ForegroundColor Cyan
+    $null = Set-Location $env_path
+    $abs_path = Get-Location
+    
+    try {
+        Write-Host "  - Initializing git repository..." -ForegroundColor Cyan
+        $null = git init
+        
+        Write-Host "  - Building Python environment..." -ForegroundColor Cyan
+        $null = pyenv exec python -m virtualenv .venv
+        $venv_exists = $true
+        Write-Host "      Created .\.venv\" -ForegroundColor DarkCyan
+        $null = . .\.venv\Scripts\activate
+        $null = pip install --upgrade pip
+        Write-Host "      Upgraded pip" -ForegroundColor DarkCyan
+        $null = pip install poetry
+        Write-Host "      Installed poetry" -ForegroundColor DarkCyan
+        $null = poetry init -n `
+                --name src `
+                --python $python_version `
+                --author "$first_name $last_name <$first_name.$last_name@data-vault.com>" `
+                --description `
+                "Project ID #${env_num}: Authored by $first_name $last_name in association with Business Thinking Ltd. (dba Datavault)" `
+    
+        Write-Host "      Initialized project" -ForegroundColor DarkCyan
+        $null = poetry install
+        Write-Host "      Locked pyproject.toml" -ForegroundColor DarkCyan
+        $is_successful = $true
     }
-    Write-Host "      returning to init_dir" -ForegroundColor Cyan
-    $null = Set-Location $init_dir
-
-    if ($is_successful) {
-        $null = code $abs_path
-        Write-Host "Done!" -ForegroundColor Green
+    catch {
+        $error_message = $Error[0].Exception.Message
     }
-    else {
-        Write-Host "  - Restoring..." -ForegroundColor Cyan
-        $null = Remove-Item  $abs_path -Recurse -Force
-        if (!$error_message) { $error_message = "An unknown error occurred"}
-        Write-Warning -Message "$error_message"
+    finally {
+        Write-Host "  - Cleaning up..." -ForegroundColor Cyan
+        if ($venv_exists){
+            Write-Host "      Deactivating python virtual environment" -ForegroundColor Cyan
+            $null = deactivate
+        }
+        Write-Host "      returning to init_dir" -ForegroundColor Cyan
+        $null = Set-Location $init_dir
+    
+        if ($is_successful) {
+            $null = code $abs_path
+            Write-Host "Done!" -ForegroundColor Green
+        }
+        else {
+            Write-Host "  - Restoring..." -ForegroundColor Cyan
+            $null = Remove-Item  $abs_path -Recurse -Force
+            if (!$error_message) { 
+                $error_message = "An unknown error occurred"
+            }
+            Write-Warning -Message "$error_message"
+        }
     }
 }
 
-### ~~~~ INFO FLAGS ~~~~ ###
-if ($Version) {
-    Write-Host $version_number
-    break
-}
-if ($Help) {
+function Show-Help{
     Write-Host `
     "~~ $script_name ~~
 Command to create a new development environment under a temporary directory.
@@ -133,37 +130,33 @@ Parameter flags can be supplied with the command to adjust the script's behaviou
     $table = @(
         [PSCustomObject]@{
             Config = '$python_version';
-            Description = 'python version';
+            Description = 'Python version';
             Value = "$python_version";
             Default = '3.11.5';
         },
         [PSCustomObject]@{
             Config = '$development_dir';
-            Description = 'Directory where development environments will be created';
+            Description = 'Directory for development environments';
             Value = "$development_dir";
             Default = '~\Development\';
         },
         [PSCustomObject]@{
             Config = '$playground_dir';
-            Description = 'python version';
+            Description = 'Directory for playground environments';
             Value = "$playground_dir";
-            Default = '"~\Playground\"';
-        }
-        [PSCustomObject]@{
-            Config = '$first_name';
-            Description = 'python version';
-            Value = "$first_name";
-            Default = 'Frankie';
-        },
-        [PSCustomObject]@{
-            Config = '$last_name';
-            Description = 'python version';
-            Value = "$last_name";
-            Default = 'Homewood';
+            Default = '~\Playground\';
         }
     ) | Format-Table
     Write-Output $table
     break
+}
+
+
+### ~~~~ RUN ~~~~ ###
+switch ($true) {
+    $Version { Write-Host $version_number }
+    $Help { Show-Help }
+    Default { Build }
 }
 
 ### ~~~~ GARBAGE COLLECTION ~~~~ ###
