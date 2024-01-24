cf85a8cebc2f7ea01115accd619f84a7e362d04b
Frankie Homewood <f.homewood@outlook.com>
2024-01-24 17:04:11 +0000

    Correct Meeting scripts open file location - Cleaned up placeholder code - Add comments

--- scripts/meeting.ps1
+++ scripts/meeting.ps1
@@ -10,7 +10,7 @@ param (
 )
 
 ### ~~~~ METADATA ~~~~ ###
-$version_number = "v0.1.2"
+$version_number = "v0.1.3"
 $script_name = 'Meeting'
 
 ### ~~~~ CONFIG ~~~~ ###
@@ -23,8 +23,11 @@ $temp_dir = $devtools_dir + ".temp\"
 $is_successful = $false
 $error_message = ''
 $date = Get-Date
-$null = Remove-Item -Recurse $temp_dir
+if ($temp_dir | Test-Path) {
+    Remove-Item -Recurse -Path $temp_dir
+}
 $null = New-Item -Path $temp_dir -ItemType Directory
+$kit_dir = $kit_dir | Resolve-Path
 if ($KIT -and !($kit_dir | Test-Path)){
         $null = New-Item -Path $kit_dir -ItemType Directory
 }
@@ -60,55 +63,76 @@ function New-Meeting {
 function New-KIT {
     try {
         Write-Host "~~~ Loading Meeting Notes ~~~" -ForegroundColor DarkGreen
-        
-        $placeholders = @()
-        $placeholders += @{ Tag = '{{ SHORT DATE }}'; Inplace = "$($date.ToString("yyyy-MM-dd"))"; }
-        $placeholders += @{ Tag = '{{ LONG DATE }}'; Inplace = "$($date.ToString("dddd, d MMM yyyy"))"; }
 
-        $team_members = Get-ChildItem $kit_dir
-
+        # Show team member selection interface
         Write-Host "Team Members:" -ForegroundColor Yellow
+        $team_members = Get-ChildItem $kit_dir
         for ($i = 0; $i -lt $team_members.Length; $i++) {
             Write-Host "[$i] - $($team_members[$i])" -ForegroundColor Yellow
         }
         Write-Host "[N] + New Team Member" -ForegroundColor Yellow
         Write-Host "Whose KIT is being recorded? - " -ForegroundColor Yellow -NoNewline
-        $team_member_id = 0 # Read-Host
+        
+        # Read and process result
+        $team_member_id = Read-Host
         if ($team_member_id -eq "n") { $team_member = New-TeamMember }
         elseif ($team_member_id -lt $team_members.Length) { $team_member = $team_members[$team_member_id] }
         else { Write-Host "Could not find team member" -ForegroundColor Red; break }
-        $placeholders += @{ Tag = '{{ TEAM MEMBER }}'; Inplace = "$team_member"; }
-        Write-Host -ForegroundColor Magenta "$team_member_id`: $team_member"
-
+        
+        
         Write-Host "  - Building notes template..." -ForegroundColor Cyan
         $null = Copy-Item "$devtools_dir/env_templates/kit_note/*" $temp_dir -Recurse
-
-        Write-Host "  - Replacing placeholder filenames..." -ForegroundColor Cyan
-        Get-ChildItem -Recurse "$temp_dir/*" | ForEach-Object { $_path = $_
-            $placeholders | ForEach-Object { $_placeholder = $_
-                if ($_path -match $_placeholder.Tag){
-                    Rename-Item -Path $_path.FullName -NewName "$_path".Replace($_placeholder.Tag, $_placeholder.Inplace)
-        }}}
         
-        Write-Host "  - Getting Last we spoke..." -ForegroundColor Cyan
+        Write-Host "  - Loading last meeting..." -ForegroundColor Cyan
         $most_recent_kit = (Get-ChildItem "$kit_dir/$team_member" | Sort-Object -Descending)[0]
 
+        # Find information from the most recent kit 
+        # And extract it into the new one
+        Write-Host "  - Extracting information from last meeting..." -ForegroundColor Cyan
         $regex = "### Check-in`n((?:.*`n)*)`n## Goals`n((?:.*`n)*)`n## Actions`n(?:(?:.*`n)*)`n### New Actions`n((?:.*`n*)*)"
-        $data =  [string]::Join("`n", (Get-Content  -Path $most_recent_kit.FullName))
+        $data =  [string]::Join("`n", (Get-Content -Path $most_recent_kit.FullName))
         $match = $data | Select-String -Pattern $regex
 
-        $placeholders += @{ Tag = '{{ LAST WE SPOKE }}'; Inplace = $match.Matches[0].Groups[1].Value.Trim(); }
-        $placeholders += @{ Tag = '{{ GOALS }}'; Inplace = $match.Matches[0].Groups[2].Value.Trim(); }
-        $placeholders += @{ Tag = '{{ PROPOSED ACTIONS }}'; Inplace = $match.Matches[0].Groups[3].Value.Trim(); }
+        # Define values to replace
+        $placeholders = @(
+            @{ Tag = '{{ SHORT DATE }}'; Inplace = "$($date.ToString("yyyy-MM-dd"))"; },
+            @{ Tag = '{{ LONG DATE }}'; Inplace = "$($date.ToString("dddd, d MMM yyyy"))"; },
+            @{ Tag = '{{ TEAM MEMBER }}'; Inplace = "$team_member"; },
+            @{ Tag = '{{ LAST WE SPOKE }}'; Inplace = $match.Matches[0].Groups[1].Value.Trim(); },
+            @{ Tag = '{{ GOALS }}'; Inplace = $match.Matches[0].Groups[2].Value.Trim(); },
+            @{ Tag = '{{ PROPOSED ACTIONS }}'; Inplace = $match.Matches[0].Groups[3].Value.Trim(); }
+        )
+
+        Write-Host "  - Replacing placeholder filenames..." -ForegroundColor Cyan
+        # For each path
+        Get-ChildItem -Recurse "$temp_dir/*" | ForEach-Object { $_path = $_
+            # And every placeholder value
+            $placeholders | ForEach-Object { $_placeholder = $_
+                # Check the name of the file
+                if ($_path -match $_placeholder.Tag){
+                    # And replace any of that placeholder value in the name
+                    Rename-Item -Path $_path.FullName -NewName "$_path".Replace($_placeholder.Tag, $_placeholder.Inplace)
+
+                    #TODO: Probably wont work if needs more than one placeholder
+        }}}
 
         Write-Host "  - Populating previous info..." -ForegroundColor Cyan
+        # For each path
         Get-ChildItem -Recurse "$temp_dir/*" | ForEach-Object { $_path = $_
+            # And every placeholder value
             $placeholders | ForEach-Object { $_placeholder = $_
+                # if the file_path is not a directory
                 if (Test-Path -Path $_path.FullName -PathType Leaf) {
+                    # or an empty value
                     if ($null -ne (Get-Content $_path.FullName)) {
-                            (Get-Content $_path.FullName).Replace($_placeholder.Tag, $_placeholder.Inplace) | Set-Content $_path.FullName
+                        # Then replace any of that placeholder value in the file content
+                        (Get-Content $_path.FullName).Replace($_placeholder.Tag, $_placeholder.Inplace) | Set-Content $_path.FullName
         }}}}
+
+        # Move transformed files to their required directory
         Copy-Item "$temp_dir/*" "$kit_dir/$team_member"
+
+        # Reassess most recent meeting
         $most_recent_kit = (Get-ChildItem "$kit_dir/$team_member" | Sort-Object -Descending)[0]
             
         if ($team_member_id -ge $team_members.Length -and $team_member_id -ne "n") {break}
