0c695f9001597c7dd6c4987e37e09c685cd79475
Frankie Homewood <f.homewood@outlook.com>
2024-01-25 16:41:14 +0000

    Add copyright header to aliases and functions

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -1,3 +1,5 @@
+### © Copyright 2024 Frankie Homewood <F.Homewood@outlook.com>
+# A functions script to group all shared functions into one file to be sourced.
 function Activate-VEnv {
     param(
         [string] $path
