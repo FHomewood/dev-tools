8065ef0722a1af274b9176c49bc03ae33d2914b9
DVFrankieHomewood <Frankie.Homewood@data-vault.com>
2024-06-07 14:29:29 +0000

    WIP

--- package/dev_tools/meeting.py
+++ package/dev_tools/meeting.py
@@ -2,7 +2,6 @@
 # A command to create templated meeting notes.
 
 import click
-import functools
 import shutil
 import os
 
@@ -10,30 +9,10 @@ from datetime import datetime
 from pathlib import Path
 
 
-notes_dir = Path.home() / "Notes/"
-kit_dir = notes_dir / "Keeping_in_touch/"
-devtools_dir = Path.home() / ".dev-tools/"
-## TODO: switch these to environment variables or config
-
-is_successful = False
-error_message = ""
-date = datetime.now()
-
-temp_dir = devtools_dir / ".temp/"
-
-
 @click.command()
 @click.option("--kit", is_flag=True, help="Keeping in Touch.")
 @click.option("--daily", is_flag=True, help="Daily tracker.")
 def cli(kit, daily):
-
-    if temp_dir.is_dir():
-        shutil.rmtree(temp_dir)
-    if kit and not kit_dir.is_dir():
-        os.mkdir(kit_dir)
-
-    os.mkdir(temp_dir.absolute())
-
     if kit:
         new_kit()
     elif daily:
@@ -43,8 +22,23 @@ def cli(kit, daily):
 
 
 def new_meeting():
+    notes_dir = Path.home() / "Notes/"
+    devtools_dir = Path.home() / ".dev-tools/"
+    ## TODO: switch these to environment variables or config
+
+    is_successful = False
+    error_message = ""
+    date = datetime.now()
+
+    temp_dir = devtools_dir / ".temp/"
+
+    if temp_dir.is_dir():
+        shutil.rmtree(temp_dir)
+
+    os.mkdir(temp_dir.absolute())
+
     try:
-        click.echo(click.style("~~~ Loading Meeting Notes ~~~", fg="green"))
+        display("~~~ Loading Meeting Notes ~~~", "green")
 
         today_dir = (
             notes_dir
@@ -58,6 +52,7 @@ def new_meeting():
         files_to_copy = (devtools_dir / "templates" / "meeting_note").glob("**/*")
         for i in files_to_copy:
             shutil.copy(i, temp_dir)
+
         placeholders = (
             ("{{ TIME STAMP }}", date.strftime("%Y-%m-%d_%H-%M-%S")),
             ("{{ LONG DATE }}", date.strftime("%A, %d %b %Y")),
@@ -72,7 +67,7 @@ def new_meeting():
                 name = name.replace(placeholder[0], placeholder[1])
             os.rename(path, path.parent / name)
 
-        click.echo(click.style("  - Populating previous info...", fg="bright_cyan"))
+        display("  - Populating previous info...", "bright_cyan")
         for path in temp_dir.glob("**/*"):
             with open(path, "r+") as _file:
                 contents = "".join(_file.readlines())
@@ -88,23 +83,107 @@ def new_meeting():
 
         is_successful = True
     except Exception as e:
-        error_message += e.message
-    finally:
-        shutil.rmtree(temp_dir)
-        if is_successful:
-            os.system(
-                f'code {today_dir.absolute()} {today_dir / date.strftime("%Y-%m-%d_%H-%M-%S.md")}'
-            )
-        else:
-            click.echo(click.style("  - Restoring...", fg="bright_cyan"))
-            if not error_message:
-                error_message = "An unknown error occurred"
-            click.echo(click.style(error_message, fg="bright_cyan"))
+        display('There was a failure.', 'cyan')
+        exception_message = getattr(e, "message", repr(e))
+
+    shutil.rmtree(temp_dir)
+    if is_successful:
+        os.system(
+            f'code {today_dir.absolute()} {today_dir / date.strftime("%Y-%m-%d_%H-%M-%S.md")}'
+        )
+    else:
+        display("  - Restoring...", "bright_cyan")
+        error_message += exception_message
+        if not error_message:
+            error_message = "An unknown error occurred"
+        display(error_message, "cyan")
 
 
 def new_kit():
-    pass
+    notes_dir = Path.home() / "Notes/"
+    kit_dir = notes_dir / "Keeping_in_touch/"
+    devtools_dir = Path.home() / ".dev-tools/"
+    ## TODO: switch these to environment variables or config
+
+    is_successful = False
+    error_message = "There was a failure:\n"
+    date = datetime.now()
+
+    temp_dir = devtools_dir / ".temp/"
+
+    if temp_dir.is_dir():
+        shutil.rmtree(temp_dir)
+    if not kit_dir.is_dir():
+        os.mkdir(kit_dir)
+
+    os.mkdir(temp_dir.absolute())
+    try:
+        is_successful = False
+        display("~~~ Loading Meeting Notes ~~~", "green")
+
+        files_to_copy = (devtools_dir / "templates" / "kit_note").glob("**/*")
+        for i in files_to_copy:
+            shutil.copy(i, temp_dir)
+
+        display("Team Members:", "bright_yellow")
+
+        team_members = kit_dir.iterdir()
+        for id, team_member in enumerate(team_members):
+            display(f"[{id}] - {team_member}", "bright_yellow")
+        display(f"[N] + New Team Member", "bright_yellow")
+
+        team_member_id = click.prompt(
+            click.style(f"Whose KIT is being recorded? - ", fg="bright_yellow"),
+            show_choices=True
+        )
+
+            # set(range(len(list(team_members)))).union({'n'})
+        placeholders = (
+            ("{{ TIME STAMP }}", date.strftime("%Y-%m-%d_%H-%M-%S")),
+            ("{{ LONG DATE }}", date.strftime("%A, %d %b %Y")),
+        )
+
+        display("  - Replacing placeholder filenames...", "bright_cyan")
+        
+        for path in temp_dir.glob("**/*"):
+            name = path.name
+            for placeholder in placeholders:
+                name = name.replace(placeholder[0], placeholder[1])
+            os.rename(path, path.parent / name)
+
+        display("  - Populating previous info...", "bright_cyan")
+        for path in temp_dir.glob("**/*"):
+            with open(path, "r+") as _file:
+                contents = "".join(_file.readlines())
+                for placeholder in placeholders:
+                    contents = contents.replace(placeholder[0], placeholder[1])
+                _file.seek(0)
+                _file.write(contents)
+                _file.truncate()
+
+        files_to_copy = temp_dir.glob("**/*")
+        for i in files_to_copy:
+            shutil.copy(i, today_dir)
+
+        is_successful = True
+    except Exception as e:
+        exception_message = getattr(e, "message", repr(e))
+    shutil.rmtree(temp_dir)
+    if is_successful:
+        os.system(
+            f'code {today_dir.absolute()} {today_dir / date.strftime("%Y-%m-%d_%H-%M-%S.md")}'
+        )
+    else:
+        display("  - Restoring...", "bright_cyan")
+        error_message += exception_message
+        if not error_message:
+            error_message = "An unknown error occurred"
+        display(error_message, "cyan")
 
 
 def new_daily():
     pass
+
+
+def display(message, color):
+    click.echo(click.style(message, fg=color))
