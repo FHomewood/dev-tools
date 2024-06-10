8f4d5fb43e81466a759a73acd46307366bf755fb
DVFrankieHomewood <Frankie.Homewood@data-vault.com>
2024-06-10 18:29:39 +0000

    Add KIT function

--- package/dev_tools/meeting.py
+++ package/dev_tools/meeting.py
@@ -4,6 +4,7 @@
 import click
 import shutil
 import os
+import re
 
 from datetime import datetime
 from pathlib import Path
@@ -58,16 +59,14 @@ def new_meeting():
             ("{{ LONG DATE }}", date.strftime("%A, %d %b %Y")),
         )
 
-        click.echo(
-            click.style("  - Replacing placeholder filenames...", fg="bright_cyan")
-        )
+        display("  - Replacing placeholder filenames...")
         for path in temp_dir.glob("**/*"):
             name = path.name
             for placeholder in placeholders:
                 name = name.replace(placeholder[0], placeholder[1])
             os.rename(path, path.parent / name)
 
-        display("  - Populating previous info...", "bright_cyan")
+        display("  - Populating previous info...")
         for path in temp_dir.glob("**/*"):
             with open(path, "r+") as _file:
                 contents = "".join(_file.readlines())
@@ -83,7 +82,7 @@ def new_meeting():
 
         is_successful = True
     except Exception as e:
-        display('There was a failure.', 'cyan')
+        display("There was a failure.", "cyan")
         exception_message = getattr(e, "message", repr(e))
 
     shutil.rmtree(temp_dir)
@@ -92,7 +91,7 @@ def new_meeting():
             f'code {today_dir.absolute()} {today_dir / date.strftime("%Y-%m-%d_%H-%M-%S.md")}'
         )
     else:
-        display("  - Restoring...", "bright_cyan")
+        display("  - Restoring...")
         error_message += exception_message
         if not error_message:
             error_message = "An unknown error occurred"
@@ -100,9 +99,9 @@ def new_meeting():
 
 
 def new_kit():
-    notes_dir = Path.home() / "Notes/"
-    kit_dir = notes_dir / "Keeping_in_touch/"
-    devtools_dir = Path.home() / ".dev-tools/"
+    notes_dir = Path.home() / "Notes"
+    kit_dir = notes_dir / "Meeting Notes" / "Keeping in Touch"
+    devtools_dir = Path.home() / ".dev-tools"
     ## TODO: switch these to environment variables or config
 
     is_successful = False
@@ -121,37 +120,68 @@ def new_kit():
         is_successful = False
         display("~~~ Loading Meeting Notes ~~~", "green")
 
+        # Copy kit notes into temp
         files_to_copy = (devtools_dir / "templates" / "kit_note").glob("**/*")
         for i in files_to_copy:
             shutil.copy(i, temp_dir)
 
+        # Show team member selection interface
         display("Team Members:", "bright_yellow")
 
-        team_members = kit_dir.iterdir()
+        team_members = list(kit_dir.iterdir())
         for id, team_member in enumerate(team_members):
-            display(f"[{id}] - {team_member}", "bright_yellow")
+            display(f"[{id}] - {team_member.name}", "bright_yellow")
         display(f"[N] + New Team Member", "bright_yellow")
 
         team_member_id = click.prompt(
             click.style(f"Whose KIT is being recorded? - ", fg="bright_yellow"),
-            show_choices=True
+            show_choices=True,
         )
 
-            # set(range(len(list(team_members)))).union({'n'})
+        # Read and process result
+        if team_member_id in ("n", "N"):
+            return new_team_member(kit_dir=kit_dir, date=date, temp_dir=temp_dir)
+        elif int(team_member_id) in range(len(team_members)):
+            team_member = team_members[int(team_member_id)]
+        else:
+            display("Could not find team member", "bright_red")
+
+        display("  - Building notes template...")
+
+        display("  - Loading last meeting...")
+        most_recent_kit = list(
+            (kit_dir / team_member).glob("*")
+        )  ## TODO: Change glob argument to only target KIT files
+        most_recent_kit.sort()
+        most_recent_kit = most_recent_kit[-1]
+
+        # Find information from the most recent kit
+        # And extract it into the new one
+        display("  - Extracting information from last meeting...")
+        with open(most_recent_kit, "r") as file:
+            data = "".join(file.readlines())
+        regex = "^(?:(?:.*\n)*)### Check-in\n((?:.*\n)*)\n## Goals\n((?:.*\n)*)\n## Actions\n(?:(?:.*\n)*)\n### Actions\n((?:.*\n*)*)"
+        match = re.match(regex, data)
+
+        # Define values to replace
         placeholders = (
             ("{{ TIME STAMP }}", date.strftime("%Y-%m-%d_%H-%M-%S")),
             ("{{ LONG DATE }}", date.strftime("%A, %d %b %Y")),
+            ("{{ SHORT DATE }}", date.strftime("%Y-%m-%d")),
+            ("{{ TEAM MEMBER }}", team_member.name),
+            ("{{ LAST WE SPOKE }}", match.group(1).strip()),
+            ("{{ GOALS }}", match.group(2).strip()),
+            ("{{ PROPOSED ACTIONS }}", match.group(3).strip()),
         )
 
-        display("  - Replacing placeholder filenames...", "bright_cyan")
-        
+        display("  - Replacing placeholder filenames...")
         for path in temp_dir.glob("**/*"):
             name = path.name
             for placeholder in placeholders:
                 name = name.replace(placeholder[0], placeholder[1])
             os.rename(path, path.parent / name)
 
-        display("  - Populating previous info...", "bright_cyan")
+        display("  - Populating previous info...")
         for path in temp_dir.glob("**/*"):
             with open(path, "r+") as _file:
                 contents = "".join(_file.readlines())
@@ -163,18 +193,91 @@ def new_kit():
 
         files_to_copy = temp_dir.glob("**/*")
         for i in files_to_copy:
-            shutil.copy(i, today_dir)
+            shutil.copy(i, team_member)
+
+        most_recent_kit = list(
+            (kit_dir / team_member).glob("*")
+        )  ## TODO: Change glob argument to only target KIT files
+        most_recent_kit.sort()
+        most_recent_kit = most_recent_kit[-1]
 
         is_successful = True
     except Exception as e:
+        display("There was a failure.", "cyan")
         exception_message = getattr(e, "message", repr(e))
+
+    display("  - Restoring...")
     shutil.rmtree(temp_dir)
     if is_successful:
-        os.system(
-            f'code {today_dir.absolute()} {today_dir / date.strftime("%Y-%m-%d_%H-%M-%S.md")}'
-        )
+        display("  - Opening notes")
+        os.system(f'code "{team_member.absolute()}" "{most_recent_kit.absolute()}"')
+        display("Done!", "bright_green")
     else:
-        display("  - Restoring...", "bright_cyan")
+        error_message += exception_message
+        if not error_message:
+            error_message = "An unknown error occurred"
+        display(error_message, "cyan")
+
+
+def new_team_member(kit_dir, date, temp_dir):
+    try:
+        team_member = click.prompt(
+            click.style(f"New team member name: ", fg="bright_yellow"),
+            show_choices=True,
+        )
+        team_member = kit_dir / team_member.title().strip()
+        os.mkdir(team_member.absolute())
+
+        # Define values to replace
+        placeholders = (
+            ("{{ TIME STAMP }}", date.strftime("%Y-%m-%d_%H-%M-%S")),
+            ("{{ LONG DATE }}", date.strftime("%A, %d %b %Y")),
+            ("{{ SHORT DATE }}", date.strftime("%Y-%m-%d")),
+            ("{{ TEAM MEMBER }}", team_member.name),
+            ("{{ LAST WE SPOKE }}", "- "),
+            ("{{ GOALS }}", "- "),
+            ("{{ PROPOSED ACTIONS }}", "- "),
+        )
+
+        display("  - Replacing placeholder filenames...")
+        for path in temp_dir.glob("**/*"):
+            name = path.name
+            for placeholder in placeholders:
+                name = name.replace(placeholder[0], placeholder[1])
+            os.rename(path, path.parent / name)
+
+        display("  - Populating previous info...")
+        for path in temp_dir.glob("**/*"):
+            with open(path, "r+") as _file:
+                contents = "".join(_file.readlines())
+                for placeholder in placeholders:
+                    contents = contents.replace(placeholder[0], placeholder[1])
+                _file.seek(0)
+                _file.write(contents)
+                _file.truncate()
+
+        files_to_copy = temp_dir.glob("**/*")
+        for i in files_to_copy:
+            shutil.copy(i, team_member)
+
+        most_recent_kit = list(
+            (kit_dir / team_member).glob("*")
+        )  ## TODO: Change glob argument to only target KIT files
+        most_recent_kit.sort()
+        most_recent_kit = most_recent_kit[-1]
+
+        is_successful = True
+    except Exception as e:
+        display("There was a failure.", "cyan")
+        exception_message = getattr(e, "message", repr(e))
+
+    display("  - Restoring...")
+    shutil.rmtree(temp_dir)
+    if is_successful:
+        display("  - Opening notes")
+        os.system(f'code "{team_member.absolute()}" "{most_recent_kit.absolute()}"')
+        display("Done!", "bright_green")
+    else:
         error_message += exception_message
         if not error_message:
             error_message = "An unknown error occurred"
@@ -185,5 +288,5 @@ def new_daily():
     pass
 
 
-def display(message, color):
+def display(message, color="bright_cyan"):
     click.echo(click.style(message, fg=color))
