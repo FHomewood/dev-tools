2ea1009d799e7d4273049af7d6224b8153351c85
Frankie Homewood <fhomewood98@gmail.com>
2024-08-09 22:50:44 +0100

    Add title notes command skeleton

---
+++ package/dev_tools/title-notes.py
@@ -0,0 +1,12 @@
+### © Copyright 2024 Frankie Homewood <F.Homewood@outlook.com>
+# A command to rename note files to have suitable titles.
+
+import click
+
+@click.command()
+def cli():
+    title_notes()
+
+def title_notes():
+    pass
+
