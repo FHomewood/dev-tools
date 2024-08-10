4d712c2d37155f1e74cb3fa36d6f851e8a7062fd
FHomewood <fhomewood98@gmail.com>
2024-08-10 18:20:07 +0100

    action function looks for and creates file

--- package/dev_tools/actions.py
+++ package/dev_tools/actions.py
@@ -5,13 +5,40 @@ import click
 import shutil
 import os
 import re
+import json
 
 from datetime import datetime
 from pathlib import Path
 
 @click.command()
 def cli():
-    title_notes()
+    actions()
 
 def actions():
-    pass
+    dev_tools_dir = Path.home() / '.dev-tools'
+    notes_dir = Path.home() / "Notes/"
+
+    is_successful = False
+    error_message = ''
+    try:
+        actions_file_path = dev_tools_dir / 'dot-files'/ '.dtactions'
+        os.makedirs(actions_file_path.parent, exist_ok=True)
+        if not actions_file_path.is_file():
+            with open(actions_file_path, 'w') as file:
+                file.write('{}')
+        actions_file = json.load(actions_file_path)
+        is_successful = True
+    except Exception as e:
+        display("There was a failure.", "cyan")
+        exception_message = getattr(e, "message", repr(e))
+    if is_successful:
+        display("Done!", "bright_green")
+    else:
+        error_message += exception_message
+        if not error_message:
+            error_message = "An unknown error occurred"
+        display(error_message, "cyan")
+
+
+def display(message, color="bright_cyan"):
+    click.echo(click.style(message, fg=color))
