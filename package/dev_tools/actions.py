ed05d3c2d7d867fa6bb5a2dcedf213bcbe132275
FHomewood <fhomewood98@gmail.com>
2024-08-11 09:36:14 +0100

    Complete base actions command

--- package/dev_tools/actions.py
+++ package/dev_tools/actions.py
@@ -10,9 +10,19 @@ import json
 from datetime import datetime
 from pathlib import Path
 
-@click.command()
-def cli():
-    actions()
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if not ctx.invoked_subcommand:
+        actions()
+
+@cli.command()
+@click.argument(
+    'ticket_pattern',
+    type=str
+)
+def close(ticket_pattern):
+    print(ticket_pattern)
 
 def actions():
     dev_tools_dir = Path.home() / '.dev-tools'
@@ -33,12 +43,12 @@ def actions():
         if not hasattr(actions_file, 'active'):
             actions_file['active'] = list()
         sub_files = notes_dir.glob('**/*.md')
-        all_actions = pull_actions_from_files(sub_files)
-        print(list(all_actions))
-
-
-
-        print(actions_file)
+        all_actions = get_all_actions(sub_files)
+        actions_file['active'] = list(all_actions.difference(set(actions_file['closed'])))
+        with open(actions_file_path, 'w') as file:
+            json.dump(actions_file, file)
+        for action in actions_file.get('active', list()):
+            display(action)
         is_successful = True
     except Exception as e:
         display("There was a failure.", "cyan")
@@ -51,12 +61,34 @@ def actions():
             error_message = "An unknown error occurred"
         display(error_message, "cyan")
 
-def pull_actions_from_files(files):
+def get_all_actions(files):
+    return set(get_action_generator(files))
+
+def get_action_generator(files):
     for file in files:
-        yield get_actions(file)
+        file_actions = get_actions(file)
+        for action in file_actions:
+            yield action
 
 def get_actions(file):
-    return file.stem, 2
+    ts_regex = r'^[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}'
+    if not re.match(ts_regex, file.stem):
+        return list()
+    with open(file, 'r') as file_reader:
+        file_contents = ''.join(file_reader.readlines())
+    timestamp = re.match(f"({ts_regex})", file.stem).group(1)
+    actions_regex = r'(?<=### Actions\n)(.*\n)*(?=\n### Tags)'
+    actions = re.search(actions_regex, file_contents)
+    if not actions:
+        return list()
+    actions = actions.group(1)
+    actions = [
+        re.match('- (.+)', action_line).group(1)
+        for action_line in actions.split('\n')
+        if re.match('- (.+)', action_line)
+        ]
+    actions = [f"{timestamp} | {action}" for action in actions]
+    return actions
 
 def display(message, color="bright_cyan"):
     click.echo(click.style(message, fg=color))
